7f96da10e5a596d20457352016502545e730ec2c
Romel Pérez <ronelprhone@gmail.com>
2016-07-05 16:27:23 -0500

    update: example

--- example.js
+++ example.js
@@ -28,23 +28,25 @@ log1.debug('message', 1);
 log1.info('message', 2);
 log1.warn('message', 3);
 log1.danger('message', 4);
-log1.error('message', 5);
-log1.fatal('message', 6);
+log1.error('message', 5);  // displayed
+log1.fatal('message', 6);  // displayed
 
 console.log('----------');
 
 log2.debug('message', 7);
-log2.info('message', 8);
-log2.warn('message', 9);
-log2.danger('message', 10);
-log2.error('message', 11);
-log2.fatal('message', 12);
+log2.info('message', 8);  // displayed
+log2.warn('message', 9);  // displayed
+log2.danger('message', 10);  // displayed
+log2.error('message', 11);  // displayed
+log2.fatal('message', 12);  // displayed
 
 console.log('----------');
 
+log1.setLevel(2);
+
 log1.debug('message', 13);
 log1.info('message', 14);
-log1.warn('message', 15);
-log1.danger('message', 16);
-log1.error('message', 17);
-log1.fatal('message', 18);
+log1.warn('message', 15);  // displayed
+log1.danger('message', 16);  // displayed
+log1.error('message', 17);  // displayed
+log1.fatal('message', 18);  // displayed
